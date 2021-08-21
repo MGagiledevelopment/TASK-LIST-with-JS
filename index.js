d757fb4834ef4162dd405b385f8ef1600e42c612
MATIAS GONZALEZ <matias.goonzalez@gmail.com>
2021-08-21 13:22:09 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const input = document.querySelector('input')
+const addButton =  document.querySelector('.btn-add')
+const ul = document.querySelector("ul")
+const empty = document.querySelector('.empty')
+
