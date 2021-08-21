6dfcd18f86665aa85ff0731e69bdefccf11c55a9
MATIAS GONZALEZ <matias.goonzalez@gmail.com>
2021-08-21 13:45:26 -0300

    funcionalidad

--- index.js
+++ index.js
@@ -3,3 +3,22 @@ const addButton =  document.querySelector('.btn-add')
 const ul = document.querySelector("ul")
 const empty = document.querySelector('.empty')
 
+
+
+addButton.addEventListener('click', (event) => {
+    event.preventDefault()
+
+
+
+    const text = input.value; //texto exscrito en input
+    const li = document.createElement('li') // creacion de li
+    const p= document.createElement('p') //creacion de p
+    p.textContent = text; // agregar contenido a p
+
+    li.appendChild(p) // a ese li creado le ponemos p que va tener un texto
+    ul.appendChild(li) //a UL le vamos agregar el LI nuevo
+
+
+
+
+})
