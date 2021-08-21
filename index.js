794b51431d457d043ed924f70348d307e24b079e
MATIAS GONZALEZ <matias.goonzalez@gmail.com>
2021-08-21 13:55:02 -0300

    delete button

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
-const input = document.querySelector('input')
-const addButton =  document.querySelector('.btn-add')
-const ul = document.querySelector("ul")
-const empty = document.querySelector('.empty')
+const input = document.querySelector("input");
+const addButton = document.querySelector(".btn-add");
+const ul = document.querySelector("ul");
+const empty = document.querySelector(".empty");
+
+addButton.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const text = input.value; //texto exscrito en input
+  const li = document.createElement("li"); // creacion de li
+  const p = document.createElement("p"); //creacion de p
+  p.textContent = text; // agregar contenido a p
+
+  li.appendChild(p); // a ese li creado le ponemos p que va tener un texto
+  li.appendChild(addDeleteButton())
+  ul.appendChild(li); //a UL le vamos agregar el LI nuevo
+});
 
 
+const addDeleteButton = () =>{
+    const deleteButton = document.createElement('button');
 
-addButton.addEventListener('click', (event) => {
-    event.preventDefault()
+    deleteButton.textContent = "X"
+    deleteButton.className = "btn-delete"
 
-
-
-    const text = input.value; //texto exscrito en input
-    const li = document.createElement('li') // creacion de li
-    const p= document.createElement('p') //creacion de p
-    p.textContent = text; // agregar contenido a p
-
-    li.appendChild(p) // a ese li creado le ponemos p que va tener un texto
-    ul.appendChild(li) //a UL le vamos agregar el LI nuevo
-
-
-
-
+ deleteButton.addEventListener('click', (element)=>{
+ const item = element.target.parentElement; //el parent elemnt hace referencia a su elemento PADRE por eso selecciona al LI !! PARA eliminarlo
+   ul.removeChild(item) 
 })
+return deleteButton;
+}
